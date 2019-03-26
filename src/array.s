862987f8fe0f180c784f5e5d81915e517fadb416
jido <denisgithub@bredelet.com>
2019-03-26 23:29:49 +0000

    Factor out byte copying
    Use in the shiftLeft function, will also go in shiftRight* and
    rotate*
    
    The type function should return a pointer to the type, created
    the newArray function instead.

--- src/array.s
+++ src/array.s
@@ -3,28 +3,40 @@
   .globl  _main
   .p2align  4
 _main:
-  lea rax, _Array_template[rip]
-  mov rax, meta[rax]
   mov rbx, rsp                        ## save stack pointer
-  sub rsp, size[rax]                  ## allocate stack space for _Array_template
+  lea rax, _Array_template[rip]
+  mov rcx, meta[rax]
+  sub rsp, size[rcx]                  ## allocate stack space for _Array_template
   lea rdi, _int32[rip]
   mov rdi, 010[rdi]                   ## _Integer_bits_32
   mov rsi, 5
   lea r8, main_next[rip]
   lea r9, main_end[rip]
-  jmp instance[rax]                   ## create an instance of Array(<itemType: Integer(<bits: 32>), size: 5>)
+  jmp newArray[rax]                   ## create an instance of Array(<itemType: Integer(<bits: 32>), size: 5>)
 main_next:
-  mov rax, rsp                        ## save address of array type
+  mov rax, rsp
+  mov r12, rax                        ## save address of array type
   sub rsp, size[rax]                  ## allocate stack space for an array
-  mov dword ptr [rsp], 56
-  mov dword ptr 4[rsp], 99
+  mov dword ptr 8[rsp], 56
+  mov dword ptr 12[rsp], 99
+  mov dword ptr 16[rsp], 107
+  sub rsp, size[rax]                  ## allocate stack space for an array
+  lea rdi, 20[rsp]
+  mov rsi, 2
+  lea r8, main_items[rip]
+  lea r9, main_end[rip]
+  jmp _shiftLeft
+main_items:
+  mov rax, r12
   mov rdi, rsp
   push rsp                            ## address of array
   lea r8, main_value[rip]
+  lea r9, main_end[rip]
   jmp value[rax]                      ## first item
 main_value:
   mov rcx, r8
   lea r8, main_value2[rip]
+  lea r9, main_end[rip]
   jmp rcx                             ## second item
 main_value2:
   mov rcx, r9
@@ -81,6 +93,19 @@ indices_end:
   mov rsp, rdi                      ## restore stack pointer
   jmp r9
 
+
+  ## copy(dest, source, times, bytes)
+copy:
+  cld
+  rep movsb
+  cmp rdx, 1
+  jle copy_end
+  sub rdx, 1
+  sub rsi, rcx
+  jmp copy
+copy_end:
+  jmp r8
+
   base  =030
   array =020
   shift =010
@@ -103,31 +128,29 @@ _shiftLeft:                         ## precondition: rsi is a number in the rang
   lea r9, shiftLeft_badItem[rip]
   jmp instance[rax]                 ## get instance of item type
 shiftLeft_start:
-  mov rcx, shift[rsp]               ## shift by that many items
-  mov r8, return[rsp]
-  mov [rsp], rax                    ## save default value of item type
-  mov 010[rsp], rdx
+  mov rsi, rax                      ## default value of item type
   mov rax, base[rsp]
-  mov rdx, itemType[rax]
-  mov rdx, size[rdx]                ## item size
-  imul rcx, rdx                     ## number of bytes
+  mov r10, size[rax]                ## array size in bytes
+  mov rax, itemType[rax]
+  mov rcx, size[rax]                ## item size
+  mov rdx, shift[rsp]               ## shift by that many items
+  lea rdi, 040[rsp]
+  mov r11, rdx
+  imul r11, rcx                     ## amplitude of shift in bytes
+  sub r10, r11                      ## remaining bytes (keepers)
+  add rdi, r10                      ## at the end of the array
+  lea r8, shiftLeft_main[rip]
+  jmp copy
+shiftLeft_main:
+  mov rcx, r10                      ## number of bytes to copy
+  mov rdx, 1                        ## no repetition
   mov rsi, array[rsp]
-  add rsi, rcx                      ## source address
-  lea r10, 040[rsp]
-  mov rdi, r10                      ## destination address
-  cld
-  rep movsb                         ## shift items
-  mov rcx, rdx
-  imul rcx, arraySize[rax]
-  add r10, rcx                      ## end marker
-shiftLeft_loop:
-  mov rcx, rdx
-  mov rsi, rsp
-  rep movsb                         ## copy default item value to free slot in array
-  cmp rdi, r10                      ## note: destination address is auto incremented
-  jl shiftLeft_loop
-  add rsp, 040
-  jmp r8
+  add rsi, r11                      ## source address
+  lea rdi, 040[rsp]                 ## destination address
+  lea r8, shiftLeft_end[rip]
+  jmp copy
+shiftLeft_end:
+  mov r9, return[rsp]
 shiftLeft_badItem:
   add rsp, 040
 shiftLeft_invalid:
@@ -179,11 +202,14 @@ _instance:
   jmp r9
 
 _type:
+  jmp r9
+
+_newArray:
   mov rdx, rdi
   mov r10, rsi
-  lea rsi, _Array_template[rip]
+  mov rsi, rax    ##_Array_template[rip]
   mov rdi, rsp
-  lea rcx, _Array_template_meta[rip]
+  mov rcx, meta[rax]  ##_Array_template_meta[rip]
   mov rcx, size[rcx]
   shr rcx, 3
   cld
@@ -206,28 +232,30 @@ _Array_template:
   meta            =010
   instance        =020
   size            =030
-  itemType        =040
-  arraySize       =050
-  get             =060
-  contains        =070
-  indexOf         =0100
-  count           =0110
-  indices         =0120
-  shiftLeft       =0130
-  shiftRight      =0140
-  shiftRightFill  =0150
-  rotateLeft      =0160
-  rotateRight     =0170
-  value           =0200
-  constantSpace   =0210
+  newArray        =040
+  itemType        =050
+  arraySize       =060
+  get             =070
+  contains        =0100
+  indexOf         =0110
+  count           =0120
+  indices         =0130
+  shiftLeft       =0140
+  shiftRight      =0150
+  shiftRightFill  =0160
+  rotateLeft      =0170
+  rotateRight     =0200
+  value           =0210
+  constantSpace   =0220
 Type:
   .quad _array                ## module
   .quad _Array_template_meta  ## metatype
   .quad _instance
   .quad 4                     ## size
 Array:
-  .quad _int32 + 010
-  .quad 5
+  .quad _newArray
+  .quad 0
+  .quad 0
 Indexed:
   .quad _get
   .quad _contains
